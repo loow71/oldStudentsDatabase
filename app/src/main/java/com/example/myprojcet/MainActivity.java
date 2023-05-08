d812b353db8a011fb7a893ea40f126ce0ac030f0
Alexey <lorrdir@gmail.com>
2023-05-08 17:58:47 +0700

    Added fragment and not working button, replaced LinearLayout with ConstraintLayout in activity_main.xml, swapped students and groups for convenience

--- app/src/main/java/com/example/myprojcet/MainActivity.java
+++ app/src/main/java/com/example/myprojcet/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myprojcet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ActivityMainBinding binding;
     ColorStateList def;
     TextView groups_tab, students_tab, select;
+    StudentsFragment studentsFragment = new StudentsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +33,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         students_tab.setOnClickListener(this);
 
         select = findViewById(R.id.select);
-        def = students_tab.getTextColors();
+        def = groups_tab.getTextColors();
+
+        setNewFragment(studentsFragment);
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.groups_tab){
+        if (view.getId() == R.id.students_tab){
             select.animate().x(0).setDuration(100);
-            groups_tab.setTextColor(Color.WHITE);
-            students_tab.setTextColor(def);
-        } else if (view.getId() == R.id.students_tab){
-            groups_tab.setTextColor(def);
             students_tab.setTextColor(Color.WHITE);
-            int size = students_tab.getWidth();
+            groups_tab.setTextColor(def);
+            setNewFragment(studentsFragment);
+        } else if (view.getId() == R.id.groups_tab){
+            students_tab.setTextColor(def);
+            groups_tab.setTextColor(Color.WHITE);
+            int size = groups_tab.getWidth();
             select.animate().x(size).setDuration(100);
+            GroupsFragment groupsFragment = new GroupsFragment();
+            setNewFragment(groupsFragment);
         }
+    }
+
+    private void setNewFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 }
